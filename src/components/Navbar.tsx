239cb3d4cc44baff51879780a68d39ae05642de0
Jose Wills <jose.wills@insurancerevolution.co.uk>
2024-03-18 19:12:49 +0000

    updates box shadow on navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,20 @@
 const Navbar = () => {
   return (
-    <nav className="fixed z-10 font-berkshire text-white text-xl right-0 p-2 rounded-l-3xl shadow-[inset_0_12px_10px_1px_rgba(0,0,0,0.3)]">
-      <ul className="flex">
-        <li className="mx-4">
+    <nav className="fixed z-10 font-berkshire text-white text-xl text-right top-2 right-0 p-2 rounded-l-3xl shadow-[inset_0px_0px_30px_rgba(0,0,0,0.3)]">
+      <ul>
+        <li className="my-8">
           <a href="#landing_page">Home</a>
         </li>
-        <li className="mx-4">
+        <li className="my-8">
           <a href="#travel">Travel</a>
         </li>
-        <li className="mx-4">
+        <li className="my-8">
           <a href="#accommodation">Accommodation</a>
         </li>
-        <li className="mx-4">
+        <li className="my-8">
           <a href="#faqs">FAQs</a>
         </li>
-        <li className="mx-4">
+        <li className="my-8">
           <a href="#rsvp">RSVP</a>
         </li>
       </ul>
