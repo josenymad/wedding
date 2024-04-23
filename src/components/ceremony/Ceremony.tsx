978ea69eb45f8adf9db15de55aaa78ee3965c082
Jose Wills <jose.wills@insurancerevolution.co.uk>
2024-04-23 12:19:24 +0100

    add house photo in rsvp routes

--- src/components/ceremony/Ceremony.tsx
+++ src/components/ceremony/Ceremony.tsx
@@ -4,25 +4,30 @@ import { useLocation } from "react-router-dom";
 import CeremonyMobileNavbar from "./CeremonyMobileNavbar";
 import CeremonyNavbar from "./CeremonyNavbar";
 import RsvpForm from "../RsvpForm";
+import house from "../../assets/house.jpg";
+import { RxHamburgerMenu } from "react-icons/rx";
 
 const Ceremony = () => {
   const { pathname } = useLocation();
 
   return (
     <div className="bg-green-900 min-h-screen text-white">
-      {window.innerWidth > 1000 ? <CeremonyNavbar /> : <CeremonyMobileNavbar />}
       <Link to="/ceremony">
         <h2 className="text-[9vw] font-berkshire p-2">Ceremony</h2>
       </Link>
-      {pathname === "/ceremony" ? (
+      {window.innerWidth > 1000 ? <CeremonyNavbar /> : <CeremonyMobileNavbar />}
+      {pathname === "/ceremony" && (
         <div className="font-cherry text-xl max-w-[90vw]">
           <h3 className="p-2">
             Whooop, we're getting married! We can't wait to share our special
             day with you.
           </h3>
           <p className="p-2">
-            You'll find all the information you need by navigating the menu on
-            this page (3 lines on the top right for mobile).
+            You'll find all the information you need by navigating this page's
+            menu
+            {" ("}
+            {<RxHamburgerMenu className="inline" />}
+            {" on mobile)"}.
           </p>
 
           <p className="p-2">
@@ -34,8 +39,13 @@ const Ceremony = () => {
           </p>
           <RsvpForm formLink="https://forms.office.com/e/UDPV9Mi2zP" />
         </div>
-      ) : null}
+      )}
       <Outlet />
+      {pathname == "/ceremony" || pathname == "/ceremony/rsvp" ? (
+        <div className="max-w-xl m-auto p-4">
+          <img src={house} alt="house" className="rounded-2xl shadow-lg" />
+        </div>
+      ) : null}
     </div>
   );
 };
--- src/components/evening/Evening.tsx
+++ src/components/evening/Evening.tsx
@@ -4,24 +4,28 @@ import { useLocation } from "react-router-dom";
 import EveningNavbar from "./EveningNavbar";
 import EveningMobileNavbar from "./EveningMobileNavbar";
 import RsvpForm from "../RsvpForm";
+import house from "../../assets/house.jpg";
+import { RxHamburgerMenu } from "react-icons/rx";
 
 const Evening = () => {
   const { pathname } = useLocation();
 
   return (
     <div className="bg-green-900 min-h-screen text-white">
-      {window.innerWidth > 1000 ? <EveningNavbar /> : <EveningMobileNavbar />}
       <Link to="/evening">
         <h2 className="text-[9vw] font-berkshire p-2">Evening reception</h2>
       </Link>
-      {pathname === "/evening" ? (
+      {window.innerWidth > 1000 ? <EveningNavbar /> : <EveningMobileNavbar />}
+      {pathname === "/evening" && (
         <div className="font-cherry text-xl max-w-[90vw]">
           <h3 className="p-2">
             Whooop, we're getting married! We can't wait to party with you.
           </h3>
           <p className="p-2">
-            You'll find all the information you need by navigating the menu on
-            this page (3 lines on the top right for mobile).
+            You'll find all the information you need by navigating this pages's
+            menu{" ("}
+            {<RxHamburgerMenu className="inline" />}
+            {" on mobile)"}.
           </p>
           <p className="p-2">
             If you need to get in touch, you can also email us{" "}
@@ -32,8 +36,13 @@ const Evening = () => {
           </p>
           <RsvpForm formLink="https://forms.office.com/e/1egLYrk0S1" />
         </div>
-      ) : null}
+      )}
       <Outlet />
+      {pathname == "/evening" || pathname == "/evening/rsvp" ? (
+        <div className="max-w-xl m-auto p-4">
+          <img src={house} alt="house" className="rounded-2xl shadow-lg" />
+        </div>
+      ) : null}
     </div>
   );
 };
